a895ad4af8ac38d9b8588d17b266c7944e2a848c
foocoder <slow295185031@gmail.com>
2017-03-04 17:42:58 +0800

    add Choir

--- Choir/Choir.cpp
+++ Choir/Choir.cpp
@@ -16,20 +16,46 @@
 
 using namespace std;
 
+void GetRLIS(const vector<int> & vNumbers, vector<int> & vDP){
+    int iLen = vNumbers.size();
+    for( int i=iLen-1; i>=0; --i ){
+        vDP[i] = 1;
+        for( int j=iLen-1; j>i; --j ){
+            if( vNumbers[i] > vNumbers[j] && vDP[i] < vDP[j]+1 )
+                vDP[i] = vDP[j] + 1;
+        }
+    }
+}
+void GetLIS(const vector<int> & vNumbers, vector<int> & vDP){
+    int iLen = vNumbers.size();
+    for( int i=0; i<iLen; ++i ){
+        vDP[i] = 1;
+        for( int j=0; j<i; ++j ){
+            if( vNumbers[i] > vNumbers[j] && vDP[i] < vDP[j]+1 )
+                vDP[i] = vDP[j] + 1;
+        }
+    }
+}
 int main(int argc, char *argv[])
 {
     // 130 150 160 186 186 197 200 200
-    string strLine;
-    while( getline(cin, strLine)){
-        istringstream ssLine( strLine );
+    /* while( getline(cin, strLine)){ */
         int iNum;
-        ssLine>>iNum;
+        cin>>iNum;
         vector<int> vNumbers( iNum );
         for( int i=0; i<iNum; ++i){
-            ssLine >> vNumbers[i];
+            cin >> vNumbers[i];
         }
-        sort( vNumbers.begin(), vNumbers.end());
-    }
+        vector<int> vAscendingDP(iNum), vDescendingDP(iNum);
+        GetLIS(vNumbers, vAscendingDP);
+        GetRLIS(vNumbers, vDescendingDP);
+        int iMaxLen = 0;
+        for( int i=0; i<iNum; ++i ){
+            if(vAscendingDP[i]+vDescendingDP[i]-1 > iMaxLen)
+                iMaxLen = vAscendingDP[i]+vDescendingDP[i]-1;
+        }
+        cout<<iNum - iMaxLen<<endl;
+    /* } */
 
     return 0;
 }
