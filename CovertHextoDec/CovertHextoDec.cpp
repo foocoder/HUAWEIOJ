a01e730537a02e2a4353297295d4288b152a6001
foocoder <slow295185031@gmail.com>
2017-03-23 22:26:30 +0800

    add convert hex to dec

---
+++ CovertHextoDec/CovertHextoDec.cpp
@@ -0,0 +1,19 @@
+// ---- Program Info Start----
+//FileName:     CovertHextoDec.cpp
+//
+//Author:       Fuchen Duan
+//
+//Email:        slow295185031@gmail.com
+//
+//CreatedAt:    2017-03-23 22:23:05
+// ---- Program Info End  ----
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int nNum;
+    scanf("0x%x", &nNum);
+    printf("%d", nNum);
+    return 0;
+}
