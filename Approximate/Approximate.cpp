039e2791fd093d4d57c8a58cf26df927b3053f5f
foocoder <slow295185031@gmail.com>
2017-03-24 11:57:51 +0800

    add approximate

---
+++ Approximate/Approximate.cpp
@@ -0,0 +1,27 @@
+// ---- Program Info Start----
+//FileName:     Approximate.cpp
+//
+//Author:       Fuchen Duan
+//
+//Email:        slow295185031@gmail.com
+//
+//CreatedAt:    2017-03-24 11:35:51
+// ---- Program Info End  ----
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    float dNumber;
+    scanf( "%f", &dNumber );
+    printf( "%.0f", dNumber );
+    /* int nInteger; */
+    /* int nLeft = ( int ) dNumber; */
+    /* nLeft = dNumber > 0 ? nLeft : nLeft - 0; */
+    /* if( dNumber - nLeft >= nLeft +1 -dNumber) */
+    /*     printf( "%d", nLeft+1 ); */
+    /* else */
+    /*     printf( "%d", nLeft ); */
+
+    return 0;
+}
