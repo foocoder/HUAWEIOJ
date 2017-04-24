fdf0e6543c424adb85d56c5b101dedc74c05fafa
foocoder <slow295185031@gmail.com>
2017-04-24 21:33:56 +0800

    add RMB Trans

--- RMBTrans/RMBTrans.cpp
+++ RMBTrans/RMBTrans.cpp
@@ -6,7 +6,7 @@
  * @github:      https://foocoder.github.com
  * @homepage:    http://foocder.github.io
  * @create at:   2017-04-24 11:17:03
- * @last update: 2017-04-24 14:17:39
+ * @last update: 2017-04-24 21:25:22
  *@@@@@@@@@@ File Information Finish @@@@@@@@@@
 */
 
@@ -45,12 +45,80 @@ string strRMBUnitArr[] = {
     "分",
 };
 
+void fnProcessUnit( int nUnit, string & strChar, int nLevel, bool bStart ){
+    int nPower;
+    switch( nLevel ){
+        case 0:
+            {
+                nPower = 1;
+                int nDigit = nUnit / nPower;
+                strChar += strRMB1stArr[nDigit];
+                return;
+            }
+        case 1:
+            {
+                nPower = 10;
+                break;
+            }
+        case 2:
+            {
+                nPower = 100;
+                break;
+            }
+        case 3:
+            {
+                nPower = 1000;
+                break;
+            }
+    }
+    int nDigit = nUnit / nPower;
+    int nNext = nUnit % nPower;
+
+    //特殊处理
+    if( nLevel == 1 && !bStart && nDigit == 1 ){
+        if( nNext != 0 ){
+            strChar += strRMB2ndArr[0] + strRMB1stArr[nNext];
+        }
+        else{
+            strChar += strRMB2ndArr[0];
+        }
+        return;
+    }
+    if( nDigit != 0 && nNext != 0 ){
+        strChar += strRMB1stArr[nDigit] + strRMB2ndArr[nLevel-1];
+        fnProcessUnit( nNext, strChar, nLevel-1, true );
+    }
+    else if( nDigit == 0 && nNext != 0 ){
+        if( !bStart ){
+            fnProcessUnit( nNext, strChar, nLevel-1, bStart );
+        }
+        else{
+            if( nLevel == 2 && nNext / 10 == 0 ){
+                fnProcessUnit( nNext, strChar, nLevel-1, bStart );
+            }
+            else{
+                strChar += strRMB1stArr[0];
+                fnProcessUnit( nNext, strChar, nLevel-1, bStart );
+            }
+        }
+
+    }
+    else if( nDigit != 0 && nNext ==0 ){
+        strChar += strRMB1stArr[nDigit] + strRMB2ndArr[nLevel-1];
+    }
+}
+
 int main(int argc, char *argv[])
 {
 
     double fInput;
+    string output;
     cin>>fInput;
     cout<<"人民币";
+    if( fInput < EPSILON ){
+        cout<<"零元整"<<endl;
+        return 0;
+    }
     fInput += 1e-4;
     //处理角分
     long lInt = fInput;
@@ -90,9 +158,36 @@ int main(int argc, char *argv[])
             }
         }
     }
-    cout<<strTail<<endl;
+    /* cout<<strTail<<endl; */
 
     //处理元
+    if( lInt == 0 ){
+        output += strTail;
+    }
+    else{
+        int nOnes = lInt % 10000;
+        lInt = lInt / 10000;
+        int nWan = lInt % 10000;
+        int nYi  = lInt / 10000;
+        string strHead;
+        string strChar;
+        if( nYi != 0 ){
+            fnProcessUnit( nYi, strChar, 3, false );
+            strHead += strChar + strRMB3rdARR[1];
+            strChar.clear();
+        }
+        if( nWan != 0 ){
+            fnProcessUnit( nWan, strChar, 3, false );
+            strHead += strChar + strRMB3rdARR[0];
+            strChar.clear();
+        }
+        if( nOnes !=0 ){
+            fnProcessUnit( nOnes, strChar, 3, false );
+            strHead += strChar + "元";
+        }
+        output += strHead + strTail;
+    }
+    cout<<output<<endl;
 
     return 0;
 }
